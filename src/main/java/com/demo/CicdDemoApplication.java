a78ea7a952d627f0dd36a63c4fc033ed88b7af35
lekhap2 <102135233+lekhap2@users.noreply.github.com>
2023-03-16 20:21:02 +0530

    Update CicdDemoApplication.java

--- src/main/java/com/demo/CicdDemoApplication.java
+++ src/main/java/com/demo/CicdDemoApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CicdDemoApplication {
 
 	public static void main(String[] args) {
+		
+		
+		
 		SpringApplication.run(CicdDemoApplication.class, args);
 		
 	}
